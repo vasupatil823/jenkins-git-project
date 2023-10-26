8551ae9b67f3c39fde3b9bd8acd6e49a3163f77f
vasupatil823 <52465791+vasupatil823@users.noreply.github.com>
2023-10-27 00:01:29 +0530

    Update JenkinsTest.java
    new code

--- JenkinsTest.java
+++ JenkinsTest.java
@@ -3,5 +3,6 @@ import java.util.Date;
 public class JenkinsTest {
 public static void main(String[] args) {
 	System.out.println("Today date is :: " + new Date());
+	System.out.println("This is a code in git repository");
 }
 }
